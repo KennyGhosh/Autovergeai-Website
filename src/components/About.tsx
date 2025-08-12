abb9259e2cd743e47c962cbf05ae930fc9c17935
KennyGhosh <kennyghosh008@gmail.com>
2025-08-13 00:34:11 +0530

    Updated About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -25,7 +25,7 @@ export const About: React.FC = () => {
   <img
     src="/2.jpg"
     alt="Kenny Ghosh"
-    className="w-full h-full object-contain scale-140"
+    className="w-full h-full object-contain scale-160"
   />
 </div>
 
